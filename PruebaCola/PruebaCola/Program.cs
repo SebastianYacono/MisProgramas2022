bd7d2af0be35b3a1537c5f94af8816f13eaa23ea
Sebastian Yacono <sebayacono@hotmail.com>
2022-10-10 16:03:40 -0300

    Version 2. Añadido SortedList, Hashtable y SortedSet/HashSet

--- PruebaCola/PruebaCola/Program.cs
+++ PruebaCola/PruebaCola/Program.cs
@@ -31,7 +31,7 @@ namespace PruebaCola
 
             //Console.ReadKey();
 
-            #endregion  
+            #endregion
 
             #region Pila
 
@@ -56,54 +56,135 @@ namespace PruebaCola
 
             //Console.ReadKey();
 
-            #endregion  
+            #endregion
 
             #region ArrayList
 
-            string sumarALista;
+            //string sumarALista;
 
-            List<string> lista = new List<string>();
+            //List<string> lista = new List<string>();
 
-            Console.WriteLine("\n Ingrese un texto a almacenar en la lista\n");
-            sumarALista = Console.ReadLine();
-            lista.Add("");
+            //Console.WriteLine("\n Ingrese un texto a almacenar en la lista\n");
+            //sumarALista = Console.ReadLine();
+            //lista.Add("HOLA SOY UNA TRAMPA");
 
 
-            while (sumarALista != "")
+            //while (sumarALista != "")
+            //{
+            //    lista.Add(sumarALista);
+            //    Console.WriteLine("\n---------------------------------------\n\nIngrese otro texto a almacenar en la lista, o presione ENTER para terminar\n");
+            //    sumarALista = Console.ReadLine();
+
+            //}
+            //Console.Clear();
+            //for (int i = 1; ; i++)
+            //{
+            //    int posicion;
+            //    Console.WriteLine("\nIndique la posición que quiere leer\n");
+            //    posicion = int.Parse(Console.ReadLine());
+
+            //    if (posicion != 0)
+            //    {
+            //        if (posicion < lista.Count)
+            //        {
+            //            string frase = lista[posicion];
+            //            Console.WriteLine("\n");
+            //            Console.WriteLine(frase);
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("\n\nNo es una opción válida");
+            //        }
+
+            //    }
+            //    else
+            //    {
+            //        Console.Clear();
+            //        Console.WriteLine("\n\nPULSE CUALQUIER TECLA PARA TERMINAR DE EJECUTAR");
+            //        break; 
+            //    }
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            #region SortedList
+
+            //SortedList<string,string> diccio = new SortedList<string,string>();
+
+            //diccio.Add("hello", "hola");
+            //diccio.Add("hi", "hola");
+            //diccio.Add("bye", "adiós");
+            //diccio.Add("i", "yo");
+            //diccio.Add("I", "yo");
+            //diccio.Add("am", "soy");
+            //diccio.Add("goodbye", "adiós");
+
+
+            //Console.WriteLine("Write a phrase or word.\n");
+            //string cadena = Console.ReadLine();
+            //Console.WriteLine("\n");
+            //string[] palabras = cadena.Split(new char[] { ' ' });
+
+
+            //foreach (string s in palabras)
+            //{
+            //    if (diccio.ContainsKey(s))
+            //    {
+            //        Console.WriteLine(s + " = " + diccio[s]);
+            //    }
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            #region Hashtable
+
+            //Dictionary<string, string> diccio = new Dictionary<string, string>();
+
+            //diccio.Add("hello", "hola");
+            //diccio.Add("hi", "hola");
+            //diccio.Add("bye", "adiós");
+            //diccio.Add("i", "yo");
+            //diccio.Add("I", "yo");
+            //diccio.Add("am", "soy");
+            //diccio.Add("goodbye", "adiós");
+
+
+            //Console.WriteLine("Write a phrase or word.\n");
+            //string cadena = Console.ReadLine();
+            //Console.WriteLine("\n");
+            //string[] palabras = cadena.Split(new char[] { ' ' });
+
+
+            //foreach (string s in palabras)
+            //{
+            //    if (diccio.ContainsKey(s))
+            //    {
+            //        Console.WriteLine(s + " = " + diccio[s]);
+            //    }
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            #region SortedSet y HashSet
+
+            string cadena;
+
+            SortedSet<string> datos = new SortedSet<string>();
+
+            Console.WriteLine("Ingrese una frase");
+            cadena = Console.ReadLine();
+
+            while (cadena != "")
             {
-                lista.Add(sumarALista);
-                Console.WriteLine("\n---------------------------------------\n\nIngrese otro texto a almacenar en la lista, o presione ENTER para terminar\n");
-                sumarALista = Console.ReadLine();
-
-            }
-            Console.Clear();
-            for (int i = 1; ; i++)
-            {
-                int posicion;
-                Console.WriteLine("\nIndique la posición que quiere leer\n");
-                posicion = int.Parse(Console.ReadLine());
-
-                if (posicion != 0)
+                if (datos.Contains(cadena))
                 {
-                    if (posicion < lista.Count)
-                    {
-                        string frase = lista[posicion];
-                        Console.WriteLine("\n");
-                        Console.WriteLine(frase);
-                    }
-                    else
-                    {
-                        Console.WriteLine("\n\nNo es una opción válida");
-                    }
-                    
+                    Console.WriteLine("\nESTA FRASE YA EXISTE\n---------------\n");
                 }
-                else
-                {
-                    Console.WriteLine("\n\nPULSE UNA TECLA PARA TERMINAR DE EJECUTAR");
-                    break; 
-                }
+                datos.Add(cadena);
+                Console.WriteLine("\nIngrese siguente frase\n");
+                cadena = Console.ReadLine();
             }
-            Console.ReadKey();
+
             #endregion
 
         }
